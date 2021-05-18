005acd89c6ed938a38cf78965cf3b735f15626a5
ismael <ismaelmarquez@utexas.edu>
2021-05-18 14:57:35 -0500

    Lighting: change lighting caculations from world space to view space

--- OpenGL/LearnOpenGL/objectFS.glsl
+++ OpenGL/LearnOpenGL/objectFS.glsl
@@ -1,12 +1,14 @@
 #version 330 core
 in vec3 Normal;
 in vec3 FragPos;
+in vec3 LightPos;
+
 out vec4 FragColor;
   
 uniform vec3 objectColor;
 uniform vec3 lightColor;
-uniform vec3 lightPos;
-uniform vec3 viewPos;
+//uniform vec3 lightPos;
+//uniform vec3 viewPos;
 
 void main()
 {
@@ -15,7 +17,7 @@ void main()
     vec3 ambient = ambientIntensity * lightColor;
 
     // diffues light
-    vec3 lightDir = normalize(lightPos - FragPos);
+    vec3 lightDir = normalize(LightPos - FragPos);
     vec3 norm = normalize(Normal);
     float diffuseStrength = max(dot(norm, lightDir), 0.0);
     vec3 diffuse = diffuseStrength * lightColor;
@@ -23,7 +25,7 @@ void main()
     // specular light
     float specularStrength = 0.5;
     int shininess = 32;
-    vec3 viewDir = normalize(viewPos - FragPos);
+    vec3 viewDir = normalize(-FragPos); // the viewer is always at (0,0,0) in view-space, so viewDir is (0,0,0) - Position => -Position
     vec3 reflectionDir = reflect(-lightDir, norm);
     float spec = pow(max(dot(viewDir, reflectionDir), 0.0), shininess);
     vec3 specular = specularStrength * spec * lightColor;
--- OpenGL/LearnOpenGL/objectVS.glsl
+++ OpenGL/LearnOpenGL/objectVS.glsl
@@ -4,15 +4,18 @@ layout (location = 1) in vec3 aNormal;
 
 out vec3 FragPos;
 out vec3 Normal;
+out vec3 LightPos;
 
+uniform vec3 lightPos;
 uniform mat4 model;
 uniform mat4 view;
 uniform mat4 projection;
 
 void main()
 {
-    FragPos = vec3(model * vec4(aPos, 1.0));
-    mat3 NormalMatrix = mat3(transpose(inverse(model)));
+    FragPos = vec3(view * model * vec4(aPos, 1.0));
+    mat3 NormalMatrix = mat3(transpose(inverse(view * model)));
     Normal = NormalMatrix * aNormal;
-    gl_Position = projection * view * vec4(FragPos, 1.0);
+    gl_Position = projection * vec4(FragPos, 1.0);
+    LightPos = vec3(view * vec4(lightPos, 1.0));
 }
