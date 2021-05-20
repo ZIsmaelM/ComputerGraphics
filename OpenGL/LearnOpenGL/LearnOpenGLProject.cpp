ae024df6e6c40025be8e1c1f51ada1c3e8adb6b3
ismael <ismaelmarquez@utexas.edu>
2021-05-20 16:57:56 -0500

    Lighting: Implement directional light

--- OpenGL/LearnOpenGL/LearnOpenGLProject.cpp
+++ OpenGL/LearnOpenGL/LearnOpenGLProject.cpp
@@ -213,6 +213,19 @@ int main()
 	glfwSetCursorPosCallback(window, mouse_callback);
 	glfwSetScrollCallback(window, scroll_callback);
 
+	glm::vec3 cubePositions[] = {
+		glm::vec3(0.0f,  0.0f,  0.0f),
+		glm::vec3(2.0f,  5.0f, -15.0f),
+		glm::vec3(-1.5f, -2.2f, -2.5f),
+		glm::vec3(-3.8f, -2.0f, -12.3f),
+		glm::vec3(2.4f, -0.4f, -3.5f),
+		glm::vec3(-1.7f,  3.0f, -7.5f),
+		glm::vec3(1.3f, -2.0f, -2.5f),
+		glm::vec3(1.5f,  2.0f, -2.5f),
+		glm::vec3(1.5f,  0.2f, -1.5f),
+		glm::vec3(-1.3f,  1.0f, -1.5f)
+	};
+
 	unsigned int VBO, objectVAO, EBO, lightVAO;
 	setupBuffers(&VBO, &objectVAO, &EBO, &lightVAO);
 
@@ -268,10 +281,10 @@ int main()
 		glm::mat4 projection = glm::mat4(1.0f);
 		projection = glm::perspective(camera.getZoom(), ASPECT_RATIO, NEAR_PLANE, FAR_PLANE);
 		updateProjectionMatrix(projection, objectShader);		
-		glm::mat4 model = glm::mat4(1.0f);
-		updateModelMatrix(model, objectShader);
+		//glm::mat4 model = glm::mat4(1.0f);
+		//updateModelMatrix(model, objectShader);
 
-		objectShader.setVec3("light.position", lightPos);
+		objectShader.setVec3("light.direction", -0.2f, -1.0f, -0.3f);
 		objectShader.setVec3("viewPos", camera.getPosition());
 		objectShader.setVec3("light.ambient", 0.8f, 0.8f, 0.8f);
 		objectShader.setVec3("light.diffuse", 0.5f, 0.5f, 0.5f);
@@ -280,12 +293,23 @@ int main()
 		objectShader.setInt("material.specular", 1);
 		objectShader.setInt("material.emission", 2);
 		objectShader.setFloat("material.shininess", 64.0f);
-		objectShader.setMat4("model", model);
+		//objectShader.setMat4("model", model);
 		objectShader.setMat4("view", view);
 		objectShader.setMat4("projection", projection);
 		glBindVertexArray(objectVAO);
-		glDrawArrays(GL_TRIANGLES, 0, 36);
 
+		for (unsigned int i = 0; i < 10; i++)
+		{
+			glm::mat4 model = glm::mat4(1.0f);
+			model = glm::translate(model, cubePositions[i]);
+			float angle = 20.0f * i;
+			model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
+			objectShader.setMat4("model", model);
+
+			glDrawArrays(GL_TRIANGLES, 0, 36);
+		}
+
+		/*
 		lightShader.use();
 		view = glm::mat4(1.0f);
 		view = camera.getView();
@@ -302,7 +326,7 @@ int main()
 		lightShader.setVec3("lampColor", 1.0f, 1.0f, 1.0f);
 		glBindVertexArray(lightVAO);
 		glDrawArrays(GL_TRIANGLES, 0, 36);
-		
+		*/
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
--- OpenGL/LearnOpenGL/objectFS.glsl
+++ OpenGL/LearnOpenGL/objectFS.glsl
@@ -9,7 +9,7 @@ uniform Material material;
 in vec2 TexCoords;
 
 struct Light {
-    vec3 position;
+    vec3 direction;
     vec3 ambient;
     vec3 diffuse;
     vec3 specular;
@@ -28,7 +28,7 @@ void main()
 
     // diffues light
     vec3 norm = normalize(Normal);
-    vec3 lightDir = normalize(light.position - FragPos);
+    vec3 lightDir = normalize(-light.direction);
     float diff = max(dot(norm, lightDir), 0.0);
     vec3 diffuse = light.diffuse * diff * vec3(texture(material.diffuse, TexCoords));
 
@@ -40,7 +40,7 @@ void main()
 
     vec3 emission = vec3(0.0);
     if (vec3(texture(material.specular, TexCoords)).x == 0.0) {
-        emission = vec3(texture(material.emission, TexCoords));
+        //emission = vec3(texture(material.emission, TexCoords));
     }
 
     vec3 result = ambient + diffuse + specular + emission;
