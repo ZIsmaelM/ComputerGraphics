83b92bdf0bf5f824e94e542d3acaee9444efe1be
ismael <ismaelmarquez@utexas.edu>
2021-06-02 16:15:41 -0500

    ModelLoading: basic implementation working

--- OpenGL/LearnOpenGL/ModelLoading/LearnOpenGLProject.cpp
+++ OpenGL/LearnOpenGL/ModelLoading/LearnOpenGLProject.cpp
@@ -5,12 +5,17 @@
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
 
+#include <assimp/Importer.hpp>
+#include <assimp/scene.h>
+#include <assimp/postprocess.h>
+
 #include <Shader.h>
 #include <Camera.h>
-#include <iostream>
+#include <Model.h>
+//#include <iostream>
 #include "stb_image.h"
 
-#include <algorithm>
+//#include <algorithm>
 
 ////////////////////////////////////////
 // Globals
@@ -294,6 +299,8 @@ int main()
 	unsigned int VBO, objectVAO, EBO, lightVAO;
 	setupBuffers(&VBO, &objectVAO, &EBO, &lightVAO);
 
+	stbi_set_flip_vertically_on_load(true);
+
 	//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 	const char* objectVSPath = "O:/GraphicsProgramming/LearningOpenGL/LearningOpenGLSolution/LearningOpenGLProject/objectVS.glsl";
 	const char* objectFSPath = "O:/GraphicsProgramming/LearningOpenGL/LearningOpenGLSolution/LearningOpenGLProject/objectFS.glsl";
@@ -305,15 +312,19 @@ int main()
 	const char* lightFSPath = "O:/GraphicsProgramming/LearningOpenGL/LearningOpenGLSolution/LearningOpenGLProject/lightFS.glsl";
 	Shader lightShader(lightVSPath, lightFSPath);
 
-	unsigned int crateTexture;
-	const char* crateTextPath = "textures/crate.png";
-	setupTextures(&crateTexture, crateTextPath);
-	unsigned int crateSpecular;
-	const char* crateSpecPath = "textures/crate_specular.png";
-	setupTextures(&crateSpecular, crateSpecPath);
-	unsigned int crateEmission;
-	const char* crateEmitPath = "textures/crate_emission.jpg";
-	setupTextures(&crateEmission, crateEmitPath);
+	//unsigned int crateTexture;
+	//const char* crateTextPath = "textures/crate.png";
+	//setupTextures(&crateTexture, crateTextPath);
+	//unsigned int crateSpecular;
+	//const char* crateSpecPath = "textures/crate_specular.png";
+	//setupTextures(&crateSpecular, crateSpecPath);
+	//unsigned int crateEmission;
+	//const char* crateEmitPath = "textures/crate_emission.jpg";
+	//setupTextures(&crateEmission, crateEmitPath);
+
+	const char* modelPath = "textures/cat/12221_Cat_v1_3.obj";
+	Model ourModel(modelPath);
+
 	////////////////////////////////////////
 	// render loop
 	////////////////////////////////////////
@@ -324,60 +335,23 @@ int main()
 		lastFrame = currentFrame;
 		processInput(window);
 		
-		glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+		glClearColor(0.3f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 		objectShader.use();
-		glm::mat4 view = glm::mat4(1.0f);
-		view = camera.getView();
+		glm::mat4 view = camera.getView();
 		objectShader.setMat4("view", view);
-		glm::mat4 projection = glm::mat4(1.0f);
-		projection = glm::perspective(camera.getZoom(), ASPECT_RATIO, NEAR_PLANE, FAR_PLANE);
+		glm::mat4 projection = glm::perspective(camera.getZoom(), ASPECT_RATIO, NEAR_PLANE, FAR_PLANE);
 		objectShader.setMat4("projection", projection);
-		glm::mat4 model = glm::mat4(1.0f);
-		objectShader.setMat4("model", model);
-
-		objectShader.setVec3("ViewPos", camera.getPosition());
-		setupMaterials(objectShader);
-		setupDirectionalLight(objectShader);
-		setupPointLight(objectShader, pointLightPositions);
-		setupSpotLight(objectShader);
-
-		glActiveTexture(GL_TEXTURE0);
-		glBindTexture(GL_TEXTURE_2D, crateTexture);
-		glActiveTexture(GL_TEXTURE1);
-		glBindTexture(GL_TEXTURE_2D, crateSpecular);
-		glActiveTexture(GL_TEXTURE2);
-		glBindTexture(GL_TEXTURE_2D, crateEmission);
-
-		glBindVertexArray(objectVAO);
-		for (unsigned int i = 0; i < 10; i++)
-		{
-			glm::mat4 model = glm::mat4(1.0f);
-			model = glm::translate(model, cubePositions[i]);
-			float angle = 20.0f * i;
-			model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
-			objectShader.setMat4("model", model);
-
-			glDrawArrays(GL_TRIANGLES, 0, 36);
-		}
 		
-		lightShader.use();
-		lightShader.setMat4("view", view);
-		lightShader.setMat4("projection", projection);
-		lightShader.setVec3("lampColor", 1.0f, 1.0f, 1.0f);
-		glBindVertexArray(lightVAO);
-		for (unsigned int i = 0; i < 4; i++)
-		{
-			glm::mat4 model = glm::mat4(1.0f);
-			pointLightPositions[0].x = sin(glfwGetTime()/2);
-			pointLightPositions[0].z = cos(glfwGetTime()/2);
-			model = glm::translate(model, pointLightPositions[i]);
-			model = glm::scale(model, glm::vec3(0.2f));
-			lightShader.setMat4("model", model);
-			lightShader.setVec3("lampColor", pointLightColors[i]);
-			glDrawArrays(GL_TRIANGLES, 0, 36);
-		}
+		glm::mat4 model = glm::mat4(1.0f);
+		model = glm::translate(model, glm::vec3(0.0f, 0.0f, 0.0f));
+		model = glm::rotate(model, -1.5f, glm::vec3(1.0f, 0.0f, 0.0f));
+		model = glm::scale(model, glm::vec3(0.01f, 0.01f, 0.01f));
+		objectShader.setMat4("model", model);
+		ourModel.Draw(objectShader);
+
+		
 		
 		glfwSwapBuffers(window);
 		glfwPollEvents();
