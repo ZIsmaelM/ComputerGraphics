77ccaa69f020af51270f01fd275bb27b11921304
ismael <ismaelmarquez@utexas.edu>
2021-06-30 01:50:16 -0500

    Basic window functionality completed

--- Learning/UCSD/CSE167X/hw3-windows/hw3-windows/main.cpp
+++ Learning/UCSD/CSE167X/hw3-windows/hw3-windows/main.cpp
@@ -1,7 +1,33 @@
 #include <iostream>
+#include <GL/glut.h>
 
-int main()
+void keyboard(unsigned char key, int x, int y)
+{
+	switch (key) {
+	case 27:
+		exit(0);
+		break;
+	}
+}
+void display(void)
+{
+	glClearColor(0.3, 0.3, 0.3, 0.0);
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	glutSwapBuffers();
+}
+
+int main(int argc, char* argv[])
 {
 	std::cout << "Hello World!" << std::endl;
+
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE);
+	glutCreateWindow("HW3: Ray-Tracer");
+
+	glutDisplayFunc(display);
+	glutKeyboardFunc(keyboard);
+	glutMainLoop();
+
 	return 0;
 }
