55f3709c3144b25c0e64e2f9227ce236b3688d9b
zimarquez <ismaelmarquez@utexas.edu>
2021-10-30 13:53:11 -0500

    ADD: diffuse shading

--- Learning/OneWeekendRaytracer/color.h
+++ Learning/OneWeekendRaytracer/color.h
@@ -17,6 +17,11 @@ void write_color(std::ostream& out, color pixel_color, int samples_per_pixel)
     g *= scale;
     b *= scale;
 
+    // Gamma correct for gamma = 2.0
+    r = sqrt(r);
+    g = sqrt(g);
+    b = sqrt(b);
+
     out << static_cast<int>(256 * clamp(r, 0.0, 0.999)) << ' '
         << static_cast<int>(256 * clamp(g, 0.0, 0.999)) << ' '
         << static_cast<int>(256 * clamp(b, 0.0, 0.999)) << '\n';
