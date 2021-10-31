783fb3ac23b877bc9726bd6d8c44a1d2b48fec12
zimarquez <ismaelmarquez@utexas.edu>
2021-10-31 18:24:07 -0500

    ADD: Defocus blur and final render (book cover)

--- Learning/OneWeekendRaytracer/camera.h
+++ Learning/OneWeekendRaytracer/camera.h
@@ -3,6 +3,8 @@
 
 #include "rtweekend.h"
 
+#define DEFOCUS 1
+
 class camera
 {
     public:
@@ -11,7 +13,9 @@ class camera
             point3 lookat,
             vec3 vup,
             double vfov,    // vertical field of view in degrees
-            double aspect_ratio
+            double aspect_ratio,
+            double aperture,
+            double focus_dist
         )
         {
             auto theta  = degrees_to_radians(vfov);
@@ -20,19 +24,29 @@ class camera
             auto viewport_width = aspect_ratio * viewport_height;
             auto focal_length = 1.0;
 
-            auto w = unit_vector(lookfrom - lookat);
-            auto u = unit_vector(cross(vup, w));
-            auto v = unit_vector(cross(w, u));
-
+            w = unit_vector(lookfrom - lookat);
+            u = unit_vector(cross(vup, w));
+            v = unit_vector(cross(w, u));
+            
             origin = lookfrom;
-            horizontal = viewport_width * u;
-            vertical = viewport_height * v;
-            lower_left_corner = origin - horizontal/2 - vertical/2 - w;
+            horizontal = focus_dist * viewport_width * u;
+            vertical = focus_dist * viewport_height * v;
+            lower_left_corner = origin - horizontal/2 - vertical/2 - focus_dist*w;
+            if (DEFOCUS) {
+                lens_radius = aperture / 2;
+            } else {
+                lens_radius = 0;
+            }
         }
 
         ray get_ray(double s, double t) const
         {
-            return ray(origin, lower_left_corner + s*horizontal + t*vertical - origin);
+            vec3 rd = lens_radius * random_in_unit_disk();
+            vec3 offset = u * rd.x() + v * rd.y();
+
+            return ray(
+                origin + offset,
+                lower_left_corner + s*horizontal + t*vertical - origin - offset);
         }
     
     private:
@@ -40,6 +54,8 @@ class camera
         point3 lower_left_corner;
         vec3 horizontal;
         vec3 vertical;
+        vec3 u, v, w;
+        double lens_radius;
 };
 
 #endif
