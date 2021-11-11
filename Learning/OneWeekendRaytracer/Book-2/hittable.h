beda16b8ae3092c8d59ac258765cf9f388208fa8
zimarquez <ismaelmarquez@utexas.edu>
2021-11-11 16:03:41 -0600

    ADD: axis-aligned bounding box creation

--- Learning/OneWeekendRaytracer/Book-2/hittable.h
+++ Learning/OneWeekendRaytracer/Book-2/hittable.h
@@ -1,6 +1,7 @@
 #ifndef HITTABLE_H
 #define HITTABLE_H
 
+#include "aabb.h"
 #include "ray.h"
 #include "rtweekend.h"
 
@@ -25,6 +26,7 @@ class hittable
 {
     public:
         virtual bool hit(const ray& r, double t_min, double t_max, hit_record& rec) const = 0;
+        virtual bool bounding_box(double time0, double time1, aabb& output_box) const = 0;
 };
 
 #endif
