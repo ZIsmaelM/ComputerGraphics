7f28ea854988c14ee4e3d5463cfd359388149da8
zimarquez <ismaelmarquez@utexas.edu>
2021-11-19 18:12:01 -0600

    ADD: diffuse light material

--- Learning/OneWeekendRaytracer/Book-2/main.cpp
+++ Learning/OneWeekendRaytracer/Book-2/main.cpp
@@ -93,25 +93,23 @@ hittable_list earth()
     return hittable_list(globe);
 }
 
-color ray_color(const ray& r, const hittable& world, int depth)
+color ray_color(const ray& r, const color& background, const hittable& world, int depth)
 {
     hit_record rec;
 
     if (depth <= 0) return color(0,0,0);
 
-    if (world.hit(r, 0.001, infinity, rec))
-    {
-        ray scattered;
-        color attenuation;
-        if (rec.mat_ptr->scatter(r, rec, attenuation, scattered))
-            return attenuation * ray_color(scattered, world, depth-1);
-        return color(0,0,0);
-    }
+    if (!world.hit(r, 0.001, infinity, rec))
+        return background;
+    
+    ray scattered;
+    color attenuation;
+    color emitted = rec.mat_ptr->emitted(rec.u, rec.v, rec.p);
 
-    vec3 unit_direction = unit_vector(r.direction());
-    auto t = 0.5*(unit_direction.y() + 1.0);
-
-    return (1.0-t)*color(1.0, 1.0, 1.0) + t*color(0.5, 0.7, 1.0);
+    if (rec.mat_ptr->scatter(r, rec, attenuation, scattered))
+        return emitted;
+    
+    return emitted + attenuation * ray_color(scattered, background, world, depth-1);
 }
 
 int main()
@@ -130,11 +128,13 @@ int main()
     point3 lookat;
     auto vfov = 40.0;
     auto aperture = 0.0;
+    color background(0,0,0);
 
     switch (0)
     {
         case 1:
             world = book_cover_scene();
+            background = color(0.70, 0.80, 1.00);
             lookfrom = point3(13,2,3);
             lookat = point3(0,0,0);
             vfov = 20.0; // zoom
@@ -143,6 +143,7 @@ int main()
         
         case 2:
             world = two_spheres();
+            background = color(0.70, 0.80, 1.00);
             lookfrom = point3(13,2,3);
             lookat = point3(0,0,0);
             vfov = 20.0;
@@ -150,17 +151,23 @@ int main()
         
         case 3:
             world = two_perlin_spheres();
+            background = color(0.70, 0.80, 1.00);
+            lookfrom = point3(13,2,3);
+            lookat = point3(0,0,0);
+            vfov = 20.0;
+            break;
+        
+        case 4:
+            world = earth();
+            background = color(0.70, 0.80, 1.00);
             lookfrom = point3(13,2,3);
             lookat = point3(0,0,0);
             vfov = 20.0;
             break;
         
         default:
-        case 4:
-            world = earth();
-            lookfrom = point3(13,2,3);
-            lookat = point3(0,0,0);
-            vfov = 20.0;
+        case 5:
+            background = color(0.0, 0.0, 0.0);
             break;
     }
 
@@ -185,7 +192,7 @@ int main()
                 auto u = (i + random_double()) / (image_width-1);
                 auto v = (j + random_double()) / (image_height-1);
                 ray r = cam.get_ray(u, v);
-                pixel_color += ray_color(r, world, max_depth);
+                pixel_color += ray_color(r, background, world, max_depth);
             }
             write_color(std::cout, pixel_color, samples_per_pixel);
         }
