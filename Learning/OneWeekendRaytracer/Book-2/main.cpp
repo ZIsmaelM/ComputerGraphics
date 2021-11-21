7a2b909107bc98aeda198f8ed57eee35859e9734
zimarquez <ismaelmarquez@utexas.edu>
2021-11-20 21:27:50 -0600

    ADD: box class

--- Learning/OneWeekendRaytracer/Book-2/main.cpp
+++ Learning/OneWeekendRaytracer/Book-2/main.cpp
@@ -1,4 +1,5 @@
 #include "aarect.h"
+#include "box.h"
 #include "camera.h"
 #include "color.h"
 #include "hittablelist.h"
@@ -123,6 +124,9 @@ hittable_list cornell_box()
     objects.add(make_shared<xz_rect>(0, 555, 0, 555, 0, white));
     objects.add(make_shared<xz_rect>(0, 555, 0, 555, 555, white));
     objects.add(make_shared<xy_rect>(0, 555, 0, 555, 555, white));
+
+    objects.add(make_shared<box>(point3(130,0,65), point3(295,165,230), white));
+    objects.add(make_shared<box>(point3(265,0,295), point3(430,330,460), white));
 
     return objects;
 }
