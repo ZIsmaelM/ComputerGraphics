2abdd0c604a5a9420b69c1ad57b6ece3e48d6ca7
zimarquez <ismaelmarquez@utexas.edu>
2021-11-27 20:14:42 -0600

    ADD: final scene and bvh bug fix

--- Learning/OneWeekendRaytracer/Book-2/bvh.h
+++ Learning/OneWeekendRaytracer/Book-2/bvh.h
@@ -30,6 +30,32 @@ class bvh_node : public hittable
         aabb box;
 };
 
+inline bool box_compare(const shared_ptr<hittable> a, const shared_ptr<hittable> b, int axis)
+{
+    aabb box_a;
+    aabb box_b;
+
+    if (!a->bounding_box(0,0,box_a) || !b->bounding_box(0,0,box_b))
+        std::cerr << "No bounding box in bvh_node constructor.\n";
+    
+    return box_a.min().e[axis] < box_b.min().e[axis];
+}
+
+bool box_x_compare (const shared_ptr<hittable> a, const shared_ptr<hittable> b)
+{
+    return box_compare(a, b, 0);
+}
+
+bool box_y_compare (const shared_ptr<hittable> a, const shared_ptr<hittable> b)
+{
+    return box_compare(a, b, 1);
+}
+
+bool box_z_compare (const shared_ptr<hittable> a, const shared_ptr<hittable> b)
+{
+    return box_compare(a, b, 2);
+}
+
 bool bvh_node::hit(const ray& r, double t_min, double t_max, hit_record& rec) const
 {
     if (!box.hit(r, t_min, t_max))
@@ -48,7 +74,7 @@ bool bvh_node::bounding_box(double time0, double time1, aabb& output_box) const
 }
 
 bvh_node::bvh_node(
-    std::vector<shared_ptr<hittable>>& src_objects,
+    const std::vector<shared_ptr<hittable>>& src_objects,
     size_t start, size_t end, double time0, double time1)
 {
     auto objects = src_objects;
@@ -87,32 +113,6 @@ bvh_node::bvh_node(
         std::cerr << "No bounding box in bvh_node constructor.\n";
     
     box = surrounding_box(box_left, box_right);
-}
-
-inline bool box_compare(const shared_ptr<hittable> a, const shared_ptr<hittable> b, int axis)
-{
-    aabb box_a;
-    aabb box_b;
-
-    if (!a->bounding_box(0,0,box_a) || !b->bounding_box(0,0,box_b))
-        std::cerr << "No bounding box in bvh_node constructor.\n";
-    
-    return box_a.min().e[axis] < box_b.min().e[axis];
-}
-
-bool box_x_compare (const shared_ptr<hittable> a, const shared_ptr<hittable> b)
-{
-    return box_compare(a, b, 0);
-}
-
-bool box_y_compare (const shared_ptr<hittable> a, const shared_ptr<hittable> b)
-{
-    return box_compare(a, b, 1);
-}
-
-bool box_z_compare (const shared_ptr<hittable> a, const shared_ptr<hittable> b)
-{
-    return box_compare(a, b, 2);
 }
 
 #endif
