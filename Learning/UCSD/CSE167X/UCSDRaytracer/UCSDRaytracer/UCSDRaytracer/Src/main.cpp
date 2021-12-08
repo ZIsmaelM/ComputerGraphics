27afbf5972ffa25b760768a10d15a53c16e35449
zimarquez <ismaelmarquez@utexas.edu>
2021-12-07 19:36:24 -0600

    Upload project dependencies

---
+++ Learning/UCSD/CSE167X/UCSDRaytracer/UCSDRaytracer/UCSDRaytracer/Src/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+// these defines must be included before the freeglut include
+#define FREEGLUT_STATIC
+#define _LIB
+#define FREEGLUT_LIB_PRAGMAS 0
+
+#include <glew.h>
+#include <glm/glm.hpp>
+#include <freeglut.h>
+#include <FreeImage.h>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
